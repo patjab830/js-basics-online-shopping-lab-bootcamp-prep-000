ea194a2a6ff18e5f6dd4dca9b18d7056762a528b
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-28 23:33:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function viewCart() {
     if ( i !== 0 ) {
       message = `${message}, `;
     }
-    if ( i === cart.length-1 ) {
+    if ( i !== 0 && i === cart.length-1 ) {
       message = `${message}, and `;
     }
     message = `${message}${cart[i].itemName} at $${cart[i].itemPrice}`;
