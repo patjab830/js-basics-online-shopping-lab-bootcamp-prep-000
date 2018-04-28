cc7c0fe7c5a9d1fe68fe000f4fe7acd5aaf6b1be
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-28 23:37:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function total() {
 function removeFromCart(item) {
   for ( var i = 0; i < cart.length; i++ ) {
     if ( cart[i].itemName === item ) {
-      cart = [...cart.slice(0, i), ...cart.slice(i+1)];
+      cart = cart.splice(i, 1);
       return cart;
     } else {
       return "That item is not in your cart.";
