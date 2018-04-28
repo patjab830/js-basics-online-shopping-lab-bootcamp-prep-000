0647f738606be97fabf559112b23e1d140bafc47
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-28 23:41:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,8 @@ function placeOrder(cardNumber) {
   if ( cardNumber === undefined ) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
+    var costTotal = total();
     cart = [];
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${costTotal}, which will be charged to the card ${cardNumber}.`;
   }
 }
